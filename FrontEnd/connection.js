604be2240abefb87260e3d4b7f46b669c39a3954
taodev974 <payet.tony.1998@gmail.com>
2026-07-31 16:09:47 +0200

    Controle de la présence des info de connexion avant envoi API

--- FrontEnd/connection.js
+++ FrontEnd/connection.js
@@ -1,27 +1,32 @@
 async function apiUsersLogin(email, password) {
-  try {
-    const response = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    });
+  if (email !== null || password !== null) {
+    showModal(
+      "Veuillez entrer un email et un mot de passe pour vous connectez !",
+    );
+  } else
+    try {
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
-    console.log("STATUS =", response.status);
+      console.log("STATUS =", response.status);
 
-    if (!response.ok) {
-      console.warn(
-        `Identifiant incorrecte ou erreur serveur, Status: ${response.status}`,
-      );
+      if (!response.ok) {
+        console.warn(
+          `Identifiant incorrecte ou erreur serveur, Status: ${response.status}`,
+        );
+        return null;
+      }
+
+      return await response.json();
+    } catch (error) {
+      console.error("Erreure lors de la connexion :", error);
       return null;
     }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Erreure lors de la connexion :", error);
-    return null;
-  }
 }
 
 const loginBtn = document.getElementById("login-btn");
@@ -32,26 +37,41 @@ loginBtn.addEventListener("click", async (e) => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  try {
-    const response = await fetch("http://localhost:5678/api/users/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    });
+  if (email !== null || password !== null) {
+    showModal(
+      "Veuillez entrer un email et un mot de passe pour vous connectez !",
+    );
+  } else
+    try {
+      const response = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
 
-    if (!response.ok) {
-      alert("Identifiants incorrects");
-      return;
+      if (!response.ok) {
+        alert("Identifiants incorrects");
+        return;
+      }
+
+      const data = await response.json();
+
+      // Stocker le token pour le mode admin
+      localStorage.setItem("token", data.token);
+
+      // Redirection vers index.html
+      window.location.href = "index.html";
+    } catch (error) {
+      console.error("Erreur :", error);
     }
-
-    const data = await response.json();
-
-    // Stocker le token pour le mode admin
-    localStorage.setItem("token", data.token);
-
-    // Redirection vers index.html
-    window.location.href = "index.html";
-  } catch (error) {
-    console.error("Erreur :", error);
-  }
 });
+
+// Modale de connexion
+function showModal(message) {
+  document.getElementById("modal-text").textContent = message;
+  document.getElementById("modal-conect").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("modal-conect").classList.add("hidden");
+}
