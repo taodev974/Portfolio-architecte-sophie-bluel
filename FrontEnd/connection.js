544867ea64477970a146aa8eb4e61b242d80be00
taodev974 <payet.tony.1998@gmail.com>
2026-07-31 17:16:27 +0200

    Ajout modal gestion erreur connexion et amélioration du code

--- FrontEnd/connection.js
+++ FrontEnd/connection.js
@@ -1,32 +1,37 @@
 async function apiUsersLogin(email, password) {
-  if (email !== null || password !== null) {
+  // Ctrl avant l'appel API
+  if (!email.trim() || !password.trim()) {
     showModal(
       "Veuillez entrer un email et un mot de passe pour vous connectez !",
     );
-  } else
-    try {
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+    return null;
+  }
+  try {
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-      console.log("STATUS =", response.status);
+    console.log("STATUS =", response.status);
 
-      if (!response.ok) {
-        console.warn(
-          `Identifiant incorrecte ou erreur serveur, Status: ${response.status}`,
-        );
-        return null;
-      }
-
-      return await response.json();
-    } catch (error) {
-      console.error("Erreure lors de la connexion :", error);
+    if (!response.ok) {
+      // console.warn(
+      //   `Identifiant incorrecte ou erreur serveur, Status: ${response.status}`,
+      // );
+      showModal(
+        "Identifiants incorrecte! Veuillez entrer un email et mot de passe valide pour vous connectez!",
+      );
       return null;
     }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Erreure lors de la connexion :", error);
+    return null;
+  }
 }
 
 const loginBtn = document.getElementById("login-btn");
@@ -34,36 +39,20 @@ const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", async (e) => {
   e.preventDefault(); // empêche le submit classique
 
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
 
-  if (email !== null || password !== null) {
-    showModal(
-      "Veuillez entrer un email et un mot de passe pour vous connectez !",
-    );
-  } else
-    try {
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
+  const data = await apiUsersLogin(email, password);
 
-      if (!response.ok) {
-        alert("Identifiants incorrects");
-        return;
-      }
+  if (!data) {
+    return;
+  }
 
-      const data = await response.json();
+  // Stocker le token pour le mode admin
+  localStorage.setItem("token", data.token);
 
-      // Stocker le token pour le mode admin
-      localStorage.setItem("token", data.token);
-
-      // Redirection vers index.html
-      window.location.href = "index.html";
-    } catch (error) {
-      console.error("Erreur :", error);
-    }
+  // Redirection vers index.html
+  window.location.href = "index.html";
 });
 
 // Modale de connexion
